d8e52167d1d57eb60d5b49a3212f9e63c12c58a4
DJFoy <dom_727@hotmail.com>
2026-02-23 19:28:11 +0000

    Rebuilt the YaynSpinner LinePresenter in GDScript such that it has a typewriter effect, the display can be forced to hurry up, then awaits user input to progress to the next line.

--- scripts/YarnSpinner/line_presenter.gd
+++ scripts/YarnSpinner/line_presenter.gd
@@ -8,10 +8,15 @@ extends Node
 @export var use_typewriter:= true
 @export var typewriter_speed:= 60
 
+var _is_revealing:= false
+var active_tween: Tween
+
 signal dialogue_complete
+signal line_complete
 
 func _ready() -> void: 
 	presenter_control.visible = false 
+	dialogue_control.request_next_line.connect(_on_next_line_requested)
 
 func on_dialogue_start_async() -> void:
 	print("Dialogue started ")
@@ -36,4 +41,46 @@ func _run_line_internal(localized_line: YarnSpinner.LocalizedLine) -> void:
 	var output_line: String = localized_line.text_without_character_name.text
 
 	line_text_label.text = output_line
-	await dialogue_control.request_next_line
+	
+	if use_typewriter:
+		await _run_typewriter(output_line)
+	else:
+		line_text_label.visible_characters = -1
+	
+	await line_complete
+
+func _run_typewriter(text: String) -> void:
+	_is_revealing = true
+	
+	line_text_label.visible_characters = 0
+	
+	var total_chars := text.length()
+	var duration := float(total_chars) / typewriter_speed
+	
+	if active_tween:
+		active_tween.kill()
+	
+	active_tween = create_tween()
+	active_tween.tween_property(
+		line_text_label,
+		"visible_characters",
+		total_chars,
+		duration
+	)
+	
+	await active_tween.finished
+	
+	_is_revealing = false
+
+func _hurry_up_line():
+	active_tween.emit_signal("finished")
+	active_tween.kill()
+	line_text_label.visible_characters = -1
+	_is_revealing = false
+
+func _on_next_line_requested():
+	print("Next line requested")
+	if _is_revealing:
+		_hurry_up_line()
+	else:
+		emit_signal("line_complete")
