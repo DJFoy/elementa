104dee64d65d54d8e714aeda14a5de07a5b9659e
DJFoy <dom_727@hotmail.com>
2026-02-27 16:45:57 +0000

    Forgot to save

--- scripts/cutscene_manager.gd
+++ scripts/cutscene_manager.gd
@@ -1,1 +1,18 @@
 extends Node
+
+signal cutscene_finished
+@onready var ui: CanvasLayer = $"../UI"
+
+func play_cutscene(sequence: Array) -> void:
+	if Global.interacting:
+		return
+	Global.interacting = true
+	await _run_sequence(sequence)
+	Global.interacting = false
+	emit_signal("cutscene_finished")
+
+func _run_sequence(sequence: Array):
+	for step in sequence:
+		match step.type:
+			"dialogue":
+				ui.start_dialogue_ui()
