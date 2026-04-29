1f59974768b8690811a3d5cfb1dfae442d6c77be
DJFoy <dom_727@hotmail.com>
2026-04-29 21:53:24 +0100

    Forgot to save before committing.

--- scripts/SaveLoad/world_state_save.gd
+++ scripts/SaveLoad/world_state_save.gd
@@ -5,3 +5,13 @@ class_name WorldStateSave
 
 static func load(slot: String = "") -> WorldStateSave:
 	return Save._load_impl("world_state", false, WorldStateSave, slot) as WorldStateSave
+
+# --------------- SAVE DATA ---------------
+
+@export var chapter: String
+@export var one_time_dialogues: Array
+@export var items_collected: Array
+@export var cutscenes: Array
+
+@export var current_scene: String
+@export var last_location: Vector2
