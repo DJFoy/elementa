961052c4aac8be0af58807623cab0f1fc2112a5c
DJFoy <dom_727@hotmail.com>
2026-06-08 20:58:41 +0100

    Implemented functions into YarnScript that can update game state variables, such as key flags, appending events to Significant Events, so that YarnSpinner is not a storage solution for game data.

--- scripts/UI/YarnSpinner/dialogue.gd
+++ scripts/UI/YarnSpinner/dialogue.gd
@@ -10,6 +10,7 @@ signal request_dialogue_end
 func _ready() -> void:
 	GameState.interacting = true
 	line_presenter.connect("dialogue_complete", _on_dialogue_complete)
+	dialogue_runner.AddCommandHandlerCallable("update_gamestate", _yarnspinner_update_gamestate)
 	_sync_game_and_yarn()
 	
 
@@ -25,3 +26,22 @@ func _sync_game_and_yarn() -> void:
 	var vars = dialogue_runner.variableStorage
 	vars.SetValue("$playerName", Global_World_State.character.name)
 	vars.SetValue("$hatedFood", Global_World_State.character.dislikes)
+	vars.SetValue("$lovedFood", Global_World_State.character.likes)
+
+func _sync_yarn_and_game() -> void:
+	var vars = dialogue_runner.variableStorage
+	Global_World_State.familiar = vars.GetVariantValue("$chosen_familiar")
+
+func _yarnspinner_update_gamestate(key, value) -> void:
+	for prop in Global_World_State.get_property_list():
+		if prop.name == key:
+			if type_string(typeof(Global_World_State[key])) == "String":
+				Global_World_State[key] = value
+				print("Set the following key, ", key, " as this value: ", value)
+				return
+			if type_string(typeof(Global_World_State[key])) == "Array":
+				Global_World_State[key].append(value)
+				print("Appended %s to the array %s" % [value, key])
+				return
+	printerr("Requested key, %s, does not exist in Global_World_State" % key)
+	return
